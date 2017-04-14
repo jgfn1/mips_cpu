ebac023980a22fb44ac138fdf6dd2e004e6d0ce3
unknown <nmf2@G5C30.windows.cin.ufpe.br>
2017-04-14 18:48:19 -0300

    fixed muxes

--- mux32_4_1.sv
+++ mux32_4_1.sv
@@ -1,7 +1,7 @@
-module mux32_4_1(input logic [31:0] A, input logic [31:0] B, input logic [31:0] C, input logic [31:0] D, input logic [1:0], output logic [31:0] out_mux32_4, ALUSrcB)
+module mux32_4_1(input logic [31:0] A, input logic [31:0] B, input logic [31:0] C, input logic [31:0] D, input logic [1:0] ALUSrcB, output logic [31:0] out_mux32_4);
 		
 		always_comb
-			case (mux32_seletor) begin
+			case (ALUSrcB)
 				0: out_mux32_4 = A;
 				1: out_mux32_4 = B;
 				2: out_mux32_4 = C;

c2c1379711cce745515f5b54d347a488875562ae
Victor Aurélio <ovictoraurelio@gmail.com>
2017-04-16 19:47:41 -0300

    Uc working

--- src/UC.sv
+++ src/UC.sv
@@ -36,26 +36,10 @@ module UC (
 				default: state <= FETCH;
 			endcase
 	end
-	always_comb begin
-	
-		PCWriteCond 	<= 0;		//-fVictor
-		PCWrite 		<= 0; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
-		IorD 			<= 0;		// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
-		MemWrite 		<= 0;		// ler da mem�ria				
-		MemtoReg		<= 0; 		//-fVictor
-		IRWrite 		<= 0;		// Carregar o IR com o que est� em MemData
-		PCSource 		<= 0;		// Indica que o valor que ser� carregado no PC ser� o que vem do ALUResult
-		ALUOp			<= 0;		// Faz com que a opera��o da ALU seja a de soma
-		ALUSrcA 		<= 0;		// A fonte da porta A da ALU ser� o valor do PC (isso controla um MUX)
-		ALUSrcB 		<= 0;		// A fonte da porta B da ALU ser� o n�mero 4 (isso controla um MUX)				
-		RegWrite		<= 0;
-		RegDst			<= 0;		//-fVictor
-		//AWrite			<= 0;		//-fVictor
-		//BWrite			<= 0;		//-fVictor	
-		
+	always_comb		
 		case(state)
 			FETCH: begin
-				/*PCWriteCond 	<= 0;		//-fVictor
+				PCWriteCond 	<= 0;		//-fVictor
 				PCWrite 		<= 1; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
 				IorD 			<= 0;			// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
 				MemWrite 		<= 0;		// ler da mem�ria				
@@ -68,17 +52,17 @@ module UC (
 				RegWrite		<= 0;
 				RegDst			<= 0;		//-fVictor
 				AWrite			<= 0;		//-fVictor
-				BWrite			<= 0;		//-fVictor*/
-				PCWrite 		<= 1;
+				BWrite			<= 0;		//-fVictor
+				
+				/*PCWrite 		<= 1;
 				IRWrite 		<= 1;
 				PCSource 		<= 2'b00;
 				ALUOp			<= 2'b00;
-				ALUSrcB 		<= 2'b01;
-				
+				ALUSrcB 		<= 2'b01;*/				
 			 end
 			DECODE: begin
 
-				/*PCWriteCond 	<= 0;		//-fVictor
+				PCWriteCond 	<= 0;		//-fVictor
 				PCWrite 		<= 0; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
 				IorD 			<= 0;			// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
 				MemWrite 		<= 1;		// ler da mem�ria				
@@ -91,15 +75,15 @@ module UC (
 				RegWrite		<= 0;
 				RegDst			<= 0;		//-fVictor
 				AWrite			<= 1;		//-fVictor
-				BWrite			<= 1;		//-fVictor*/
+				BWrite			<= 1;		//-fVictor
 
-				MemWrite <= 1;				
+				/*MemWrite <= 1;				
 				AWrite = 1;
-				BWrite = 1;
+				BWrite = 1;*/
 			end
 			RTYPE: begin
 
-				/*PCWriteCond 	<= 0;		//-fVictor
+				PCWriteCond 	<= 0;		//-fVictor
 				PCWrite 		<= 0; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
 				IorD 			<= 0;			// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
 				MemWrite 		<= 1;		// ler da mem�ria				
@@ -112,15 +96,15 @@ module UC (
 				RegWrite		<= 0;
 				RegDst			<= 0;		//-fVictor
 				AWrite			<= 0;		//-fVictor
-				BWrite			<= 0;		//-fVictor*/
+				BWrite			<= 0;		//-fVictor
 
-				MemWrite <= 1;				
+				/*MemWrite <= 1;				
 				ALUSrcA <= 1; 		
 				ALUSrcB <= 2'b00;
-				ALUOp <= 2'b10;
+				ALUOp <= 2'b10;*/
 			end
 			RTYPE_CONT: begin
-				/*PCWriteCond 	<= 0;		//-fVictor
+				PCWriteCond 	<= 0;		//-fVictor
 				PCWrite 		<= 1; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
 				IorD 			<= 0;		// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
 				MemWrite 		<= 1;		// ler da mem�ria				
@@ -133,15 +117,15 @@ module UC (
 				RegWrite		<= 0;
 				RegDst			<= 1;		//-fVictor
 				AWrite			<= 0;		//-fVictor
-				BWrite			<= 0;		//-fVictor */
+				BWrite			<= 0;		//-fVictor
 
-				MemWrite <= 1;				
+				/*MemWrite <= 1;				
 				RegDst <= 1;
 				RegWrite <= 1;
-				MemtoReg <= 0;
+				MemtoReg <= 0; */
 			end
 			BEQ: begin
-				/*PCWriteCond 	<= 0;		//-fVictor
+				PCWriteCond 	<= 0;		//-fVictor
 				PCWrite 		<= 0; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
 				IorD 			<= 0;		// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
 				MemWrite 		<= 0;		// ler da mem�ria				
@@ -154,15 +138,16 @@ module UC (
 				RegWrite		<= 0;
 				RegDst			<= 0;		//-fVictor
 				AWrite			<= 0;		//-fVictor
-				BWrite			<= 0;		//-fVictor*/							
-				PCWriteCond <= 1;
+				BWrite			<= 0;		//-fVictor							
+				
+				/*PCWriteCond <= 1;
 				PCWrite <= 1;
 				ALUSrcA <= 1; 		
 				ALUSrcB <= 2'b00;
-				ALUOp <= 2'b01;
+				ALUOp <= 2'b01;*/
 			end
 			J: begin
-				/*PCWriteCond 	<= 0;		//-fVictor
+				PCWriteCond 	<= 0;		//-fVictor
 				PCWrite 		<= 0; 		// Faz com que o que o valor na entrada do pc seja realmente carregado.
 				IorD 			<= 0;		// o endere�o a ser carregado na porta "address" da mem�ria vem do ALUOut
 				MemWrite 		<= 0;		// ler da mem�ria				
@@ -176,7 +161,8 @@ module UC (
 				RegDst			<= 0;		//-fVictor
 				AWrite			<= 0;		//-fVictor
 				BWrite			<= 0;		//-fVictor*/
-				PCSource <= 2'b10;			
+				
+				//PCSource <= 2'b10;			
 			end
 /*			default: begin
 				PCWrite <= 0;
@@ -193,6 +179,5 @@ module UC (
 				PCWriteCond <= 0;
 				IRWrite <= 0;
 			end*/
-		endcase
-	end
+		endcase	
 endmodule 

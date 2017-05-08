e03c207cb218ac03ca763a8b5ed0bdbd9da56b05
Victor Aurélio <ovictoraurelio@gmail.com>
2017-05-08 10:59:46 -0300

    Switching IF ELSE for CASE on ALUControl

--- src/ALUControl.sv
+++ src/ALUControl.sv
@@ -6,24 +6,18 @@ module ALUControl(
 );
 	assign Break = (Funct == 6'h0d) ? 1'b1: 1'b0;
 	always_comb
-		if(ALUOp == 3'b000)							 //soma
-			Saida = 3'b001;
-		else if(ALUOp == 3'b001)					 //subtracao
-			Saida = 3'b010;
-		else if(ALUOp == 3'b010) begin
-				case (Funct)
-					6'h20: 		Saida = 3'b001;
-					default: 	Saida = 3'b110;
-				endcase
-				/*if(Funct == 6'h20) // soma com a fuct
-						Saida = 3'b001;
-				else if(Funct == 6'h24) // AND com a fuct
-						Saida = 3'b011;
-				else if(Funct == 6'h22) // sub com a fuct
-						Saida = 3'b010;
-				else if(Funct == 6'h26) // xor com a fuct
-						Saida = 3'b110;*/
-		end
-		else 						//soma
-			Saida = 3'b001;
+		case(ALUOp)
+			3'b000: 	Saida = 3'b001;	 // Soma
+			3'b001:		Saida = 3'b010;	 // Subtracao
+			3'b010: begin // O que será feito na ALU é definido pela FUNCT
+					case (Funct)
+						6'h20: 		Saida = 3'b001;// ADD definido na funct
+						6'h22: 		Saida = 3'b010;// SUB definido na funct				
+						6'h24: 		Saida = 3'b011;// AND definido na funct					
+						6'h26: 		Saida = 3'b110;// XOR definido na funct					
+						default: 	Saida = 3'b000;// por default, carrega A.
+					endcase				
+			end
+			default:	Saida = 3'b001; // Soma
+		endcase
 endmodule

51550b422f697fb7b3b685332663d6c4e30288cf
jgfn1 <jgfn@cin.ufpe.br>
2017-05-08 14:41:17 -0300

    Enabled the activation of the XOR function through the ALUOp signal.

--- src/ALUControl.sv
+++ src/ALUControl.sv
@@ -7,19 +7,20 @@ module ALUControl(
 	assign Break = (Funct == 6'h0d) ? 1'b1: 1'b0;
 	always_comb
 		case(ALUOp)
-			3'b000: 	Saida = 3'b001;	 // Soma
-			3'b001:		Saida = 3'b010;	 // Subtracao
-			3'b010: begin // O que será feito na ALU é definido pela FUNCT
+			3'b000: 	Saida = 3'b001;	 // Sum
+			3'b001:		Saida = 3'b010;	 // Subtraction
+			3'b010: begin // ALU operation is defined by the FUNCT field
 					case (Funct)
-						6'h20: 		Saida = 3'b001;// ADD definido na funct
-						6'h21: 		Saida = 3'b001;// ADDU, a priori e igual ao ADD, mas sera tratado de forma diferente caso aconteça um Overflow
-						6'h22: 		Saida = 3'b010;// SUB definido na funct
-						6'h23: 		Saida = 3'b010;// SUBU, a priori e igual ao SUB, mas sera tratado de forma diferente caso aconteça um Overflow
-						6'h24: 		Saida = 3'b011;// AND definido na funct					
-						6'h26: 		Saida = 3'b110;// XOR definido na funct					
-						default: 	Saida = 3'b000;// por default, carrega A.
+						6'h20: 		Saida = 3'b001;// ADD defined in funct
+						6'h21: 		Saida = 3'b001;// ADDU, at first is equal to ADD, but will be treated differently in case of Overflow
+						6'h22: 		Saida = 3'b010;// SUB defined in funct
+						6'h23: 		Saida = 3'b010;// SUBU, at first is equal to SUB, but will be treated differently in case of Overflow
+						6'h24: 		Saida = 3'b011;// AND defined in funct					
+						6'h26: 		Saida = 3'b110;// XOR defined in funct					
+						default: 	Saida = 3'b000;// Loads A, by default
 					endcase				
 			end
-			default:	Saida = 3'b001; // Soma
+			3'b011: 	Saida = 3'b110;//Xor activated through ALUOp, used in the sxori instruction
+			default:	Saida = 3'b000; // Loads A, by default
 		endcase
 endmodule

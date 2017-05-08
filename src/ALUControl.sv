8411a35a87760e8b7da4e25242bf15e45132f37b
jgfn1 <jgfn@WINDOWS.CIN.UFPE.BR>
2017-05-08 14:24:10 -0300

    Added the ADDU and SUBU instructions.

--- src/ALUControl.sv
+++ src/ALUControl.sv
@@ -12,7 +12,9 @@ module ALUControl(
 			3'b010: begin // O que será feito na ALU é definido pela FUNCT
 					case (Funct)
 						6'h20: 		Saida = 3'b001;// ADD definido na funct
-						6'h22: 		Saida = 3'b010;// SUB definido na funct				
+						6'h21: 		Saida = 3'b001;// ADDU, a priori e igual ao ADD, mas sera tratado de forma diferente caso aconteça um Overflow
+						6'h22: 		Saida = 3'b010;// SUB definido na funct
+						6'h23: 		Saida = 3'b010;// SUBU, a priori e igual ao SUB, mas sera tratado de forma diferente caso aconteça um Overflow
 						6'h24: 		Saida = 3'b011;// AND definido na funct					
 						6'h26: 		Saida = 3'b110;// XOR definido na funct					
 						default: 	Saida = 3'b000;// por default, carrega A.
